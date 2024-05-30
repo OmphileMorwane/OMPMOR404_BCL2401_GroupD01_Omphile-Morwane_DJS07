3641f5b3f662e6853fbb899f0d441f0c1bf07da3
Omphile Morwane <omphilemorwane@gmail.com>
2024-05-30 13:58:04 +0200

    Added a meme file then linked it to App jsx

---
+++ src/Components/meme.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function meme() {
+        return (
+             <main>
+                <form>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <button>Get a new meme image</button>
+                </form>
+             </main>   
+        )
+}
