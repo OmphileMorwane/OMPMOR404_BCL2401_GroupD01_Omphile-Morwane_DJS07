ab33c57c1cc9c685c0ddff8b29f7eafd6f44256c
Omphile Morwane <omphilemorwane@gmail.com>
2024-05-31 10:45:59 +0200

    Added comments to the Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 
+// Defines and exports the Header component as the default export of this module
 export default function Header () {
         return (
                 <header className="header">
+                  {/* Display the troll face image */}
                  <img 
                  src="./images/troll-face.png"
                  className="header--image" />
+                 {/* Display the title of the meme generator */}
                  <h2 className="header--title">Meme generator</h2>
+                 {/* Display the project information */}
                  <h4 className="header--project">React Course - Project 3</h4>
                 </header>
                 
